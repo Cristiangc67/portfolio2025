6606abf2e09d41fe3af37528df9c2b5d21467519
Cristian Gutierrez <cristiangutierrezcruz67@gmail.com>
2025-07-29 03:55:29 -0300

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 import { SkillsCarousel } from "./components/SkillsCarousel";
 import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
+import ScrollSmoother from "gsap/ScrollSmoother";
 
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Projects from "./components/Projects";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import About from "./sections/About";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
-  return (
-    <main className="w-full   overflow-x-hidden scroll-smooth ">
-      <Navbar />
-      <Hero />
-      <Projects />
+  const navbarPRef = useRef(null);
+  const smootherRef = useRef(null);
+  useGSAP(() => {
+    const smoother = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 2,
+    });
 
-      <div className="min-h-dvh"></div>
+    smootherRef.current = smoother;
+    ScrollTrigger.refresh();
+  }, []);
+
+  return (
+    <main id="smooth-wrapper" className="w-full  overflow-x-hidden">
+      <Navbar smoother={smootherRef} />
+      <div id="smooth-content">
+        <Hero />
+        <Projects />
+
+        <About />
+
+        <div className="min-h-dvh bg-blue-500"></div>
+      </div>
+
       <SkillsCarousel />
     </main>
   );
---
+++ src/sections/About.jsx
@@ -0,0 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
+
+const About = () => {
+  return (
+    <>
+      <section
+        id="sobre-mi"
+        className="min-h-dvh w-full  bg-amber-300 relative"
+      >
+        <div className="text-6xl z-10 absolute"> BSAS</div>
+      </section>
+    </>
+  );
+};
+
+export default About;
